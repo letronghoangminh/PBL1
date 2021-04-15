c2183abab45b75be232553a4c0952eb18515598d
hoangnhan1203 <hoangnhan1203@github.com>
2021-04-15 08:38:51 +0700

    swap added

--- NhapMang.cpp
+++ NhapMang.cpp
@@ -2,33 +2,33 @@
 
 using namespace std;
 
-void nhap(int rows, int cols, int matrix[][1001])
-{
+void nhap(int rows, int cols, int matrix[][1001]) {
   cout << "Nhap cac hang so: " << endl;
-  for (int i = 0; i < rows; i++)
-  {
-    for (int j = 0; j < cols; j++)
-    {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
       cin >> matrix[i][j];
     }
   }
 }
 
+void swapRow(arr[][1001], n, row1, ro22) {
+	for(int j = 0; j <= n; j++) {
+		tg = a[row1][j];
+		a[row1][j] = a[row2][j];
+		a[row2][j] = tg;
+	}
+}
+
 void gauss(int matrix[][1001], int n)
 {
-  for (int i = 1; i < n; i++)
-  {
-    if (matrix[i][i] == 0)
-    {
-      for (int j = i + 1; j <= n; j++)
-      {
-        if (matrix[j][i] != 0)
-        {
-          //doidong(matrix, n, i, j);
+  for (int i = 1; i < n; i++) {
+    if (matrix[i][i] == 0) {
+      for (int j = i + 1; j <= n; j++) {
+        if (matrix[j][i] != 0) {
+          swapRow(matrix, n, i, j);
           break;
         }
-        if (j > n)
-        {
+        if (j > n) {
           cout << "HPT vo so nghiem";
           return;
         }
@@ -37,8 +37,7 @@ void gauss(int matrix[][1001], int n)
   }
 }
 
-int main()
-{
+int main() {
   cout << "Nhap so an: " << endl;
   int n;
   cin >> n;
