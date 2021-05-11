40f32f79c27c63104ac4c42f115bff125188bb3d
hoangnhan1203 <hoangnhan1203@github.com>
2021-05-11 22:19:11 +0700

    almost done formating

--- methods.cpp
+++ methods.cpp
@@ -1,53 +1,12 @@
 #pragma once
 #include <iostream>
 #include <math.h>
+using namespace std;
 
-/*
- * This file contain 2 main method that solve the problem
- */
-
-void doKrameMethod() {
-	cout << "...doing krame method...";
-	
-	cout << endl;
-	system("pause");
+void doKrameMethod(double arr[][201], int n, double ex[]) {
+	cout << "hehehehhehee giai phuong trinh = krame" << endl;
 }
 
-void doGaussMethod(double arr[][200], int n){
-	cout << "...doing gauss method...";
-	double ex[n]; //mang chua nghiem
-
-	//bien doi ve ma tran tam giac
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = i + 1; j < n; j++) {
-			double m = - (arr[j][i] / arr	[i][i]);
-			for (int k = i; k < n + 1; k++) {
-				arr[j][k] += arr[i][k] * m;
-			}
-		}
-	}
-
-	cout << "Sau khi bien doi tam giac" << endl;
-	for (int i = 0; i < n; i++) {
-		cout << endl;
-		for (int j = 0; j < n + 1; j++) {
-			cout << arr[i][j] << " ";
-		}
-	}
-
-	//tim nghiem nguoc
-	// for (int i = n - 1; i >= 0; i--) {
-	// 	double s = arr[i][n];
-	// 	for (int k = i + 1; k < n; k++) {
-	// 		s -= arr[i][k] * ex[k];
-	// 	}
-	// 	ex[i] = s / arr[i][i];
-	// }
-
-	// // in nghiem
-	// for (int i = 1; i <= n; i++) {
-	// 	cout << ex[i] << " ";
-	// }
-	// cout << endl;
-	system("pause");
+void doGaussMethod(double arr[][201], int n, double ex[]){
+	cout << "hehehehehehe giai phuong trinh = gauss" << endl;
 }
---
+++ nhap_xuat.cpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+string selectionInput = "0";
+int isSelected2 = 0;
+
+void inputMenu() {
+	system("CLS"); 
+	cout	<< "******************************************" << endl
+			<< "*                                        *" << endl
+			<< "*               Nhap du lieu             *" << endl
+			<< "*                                        *" << endl
+			<< "*             1. Nhap thu cong           *" << endl
+			<< "*             2. Doc tu file             *" << endl
+			<< "*                                        *" << endl
+			<< "******************************************" << endl
+			<< endl;
+	
+	//Selection Validation
+	do {
+		isSelected2 = 0;
+		cout << "====> Nhap lua chon: ";
+		cin >> selectionInput;
+		
+		if(selectionInput == "1" || selectionInput == "2")
+			isSelected2 = 1;
+			
+	} while(!isSelected2);
+}
+
+void doFileInput(double arr[][201], int *soAn) {
+	ifstream input(".\\input.txt");
+	input >> *soAn; 
+	int h, w;
+	h = *soAn;
+	w = *soAn + 1;
+	
+	for(int i = 0; i < h; i++) {
+		for(int j = 0; j < w; j++) {
+			input >> arr[i][j];
+		}
+	}	
+}
+
+
+void doConsoleInput(double arr[][201], int soAn) {
+	
+	int h = soAn;
+	int w = soAn + 1;
+	
+	for(int i = 0; i < h; i++) {
+		for(int j = 0; j < w; j++) {
+			cin >> arr[i][j];
+		}
+	}
+}
+
+void doConsoleOutput(double ex[], int soAn) {
+	cout << "\nCac an cua he phuong trinh la:\n";
+	for(int i = 0; i < soAn; i++) {
+		cout << ex[i] << " ";
+	}
+	cout << endl;
+}
+
