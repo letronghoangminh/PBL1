3daf7be89673a59654935dbbcf9a07d584eb63e7
Minh <letronghoangminh@gmail.com>
2021-05-12 13:55:45 +0700

    nice

--- index.cpp
+++ index.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
-#include "arrayio.cpp"
+#include "nhap_xuat.cpp"
 #include "methods.cpp"
 
 using namespace std;
 
+const int ARR_SIZE = 200;
+
+int soAn = 0;
+double arrHeSo[ARR_SIZE][ARR_SIZE];
+double ex[ARR_SIZE];
+
 string selection = "0";
 int isSelected = 0;
 
+
 void index() {
-	
 	// Menu initalizing
 	system("CLS"); 
 	cout	<< "******************************************" << endl
@@ -31,30 +37,47 @@ void index() {
 		
 		if(selection == "1" || selection == "2" || selection == "3")
 			isSelected = 1;
-			
 	} while(!isSelected);
 }
 
+
 int main() {
-	int n;
-	cout << "Nhap bac cua he phuong trinh: " << endl;
-	cin >> n;
-	double arr[200][200]; 
-	cout << "Nhap cac he so: " << endl;
-	arrayInput(arr, n, n + 1);
+	
 	while(selection != "3") {
+		
 		index();
 		
+		/* Da chon phuong thuc giai, tien hanh nhap input
+		 * Input bao gom so an cua he phuong trinh (variableNumber) va bang he so cua he phuong trinh (arr)
+		 *
+		 */
+		inputMenu();
+		if(selectionInput == "1") {
+			cout << "Nhap so an: ";
+			cin >> soAn;
+			doConsoleInput(arrHeSo, soAn);
+		}
+		else if(selection == "2") {
+			doFileInput(arrHeSo, &soAn);
+		}
+		
+		//Da nhap input, tien hanh giai he phuong trinh
 		if(selection == "1"){
 			system("CLS");
-			doGaussMethod(arr, n);
+			int situation = doGaussMethod(arrHeSo, soAn, ex);
 		}
 		
 		if(selection == "2"){
 			system("CLS");
-			doKrameMethod();
+			doKrameMethod(arrHeSo, soAn, ex);
 		}
-			
+
+    // bien situation luu 3 gia tri: 2 la co nghiem, 1 la vo so nghiem
+    // 0 la vo nghiem, em nhan xu ly doan nay di
+
+		//Giai xong he phuong trinh, tien hanh xuat nghiem ra man hinh (xuat ex[])
+		doConsoleOutput(ex, soAn);
+		system("pause");
 	}
 	return 0;
 }
--- methods.cpp
+++ methods.cpp
@@ -6,19 +6,44 @@
  * This file contain 2 main method that solve the problem
  */
 
-void doKrameMethod() {
-	cout << "...doing krame method...";
+//Declaring utilities functions
+void permutateTriangle(double ex[], int n, double arr[][200]);
+void findReverseEx(double ex[], int n, double arr[][200]);
+int checkEx(double ex[], int n);
+
+
+
+//main functions
+void doKrameMethod(double arr[][200], int n, double ex[]) {
 	
-	cout << endl;
-	system("pause");
 }
 
-void doGaussMethod(double arr[][200], int n){
-	cout << "...doing gauss method...";
-	double ex[n]; //mang chua nghiem
+int doGaussMethod(double arr[][200], int n, double ex[]){
+	permutateTriangle(ex, n, arr);
+	findReverseEx(ex, n, arr);
+	int situation = checkEx(ex, n);
 
-	//bien doi ve ma tran tam giac
+	return situation;
+
+}
+
+//main functions
+
+//Utilities function
+void permutateTriangle(double ex[], int n, double arr[][200]) {
 	for (int i = 0; i < n - 1; i++) {
+		if (abs(arr[i][i]) < 0.001) {
+			for (int j = i + 1; j < n; j++) {
+				if (abs(arr[j][i]) > 0.01) {
+					for (int k = 0; k < n + 1; k++) {
+						double temp = arr[i][k];
+						arr[i][k] = arr[j][k];
+						arr[j][k] = temp;
+					}
+				}
+				break;
+			}
+		}
 		for (int j = i + 1; j < n; j++) {
 			double m = - (arr[j][i] / arr	[i][i]);
 			for (int k = i; k < n + 1; k++) {
@@ -27,27 +52,32 @@ void doGaussMethod(double arr[][200], int n){
 		}
 	}
 
-	cout << "Sau khi bien doi tam giac" << endl;
 	for (int i = 0; i < n; i++) {
-		cout << endl;
 		for (int j = 0; j < n + 1; j++) {
-			cout << arr[i][j] << " ";
+			if (abs(arr[i][j]) < 0.001) arr[i][j] = 0;
 		}
 	}
-
-	//tim nghiem nguoc
-	// for (int i = n - 1; i >= 0; i--) {
-	// 	double s = arr[i][n];
-	// 	for (int k = i + 1; k < n; k++) {
-	// 		s -= arr[i][k] * ex[k];
-	// 	}
-	// 	ex[i] = s / arr[i][i];
-	// }
-
-	// // in nghiem
-	// for (int i = 1; i <= n; i++) {
-	// 	cout << ex[i] << " ";
-	// }
-	// cout << endl;
-	system("pause");
 }
+
+void findReverseEx(double ex[], int n, double arr[][200]) {
+	for (int i = n - 1; i >= 0; i--) {
+		double s = arr[i][n];
+		for (int k = i + 1; k < n; k++) {
+			s -= arr[i][k] * ex[k];
+		}
+		ex[i] = s / arr[i][i];
+	}
+}
+
+int checkEx(double ex[], int n) {
+	for (int i = 0; i < n; i++) {
+		if (ex[i] == INFINITY || ex[i] == -INFINITY) {
+			return 0;
+		}
+		if (isnan(ex[i])) {
+			return 1;
+		}
+	}
+	return 2;
+}
+//Utilities functions
